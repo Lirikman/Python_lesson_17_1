404be99a2e2a2b0c55b176fac4306167684259cf
GolubevDA <999gothicman999@mail.ru>
2023-12-08 17:07:49 +0700

    parsing rabota19.ru

--- Parsing.py
+++ Parsing.py
@@ -3,14 +3,16 @@ from bs4 import BeautifulSoup
 
 url = 'https://rabota19.ru/vacancy/search/?&page=1'
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'lxml')
 
 data = soup.find('tr')
 name = data.find('div', class_='title14 strong').text.strip()
-salary = data.find('td', class_='title14').text.replace("от", "").replace('до', ' -')
+salary = data.find('td', class_='title14').text.replace("от", "").replace('до', ' -').strip()
 description = data.find_all('div', class_='small')[-1].text
+url = 'https://rabota19.ru' + data.find("a").get("href")
 
-#url = soup.find("div", class_="title14 strong")
-
-print(description)
 #print(data)
+print(name)
+print(salary)
+print(description)
+print(url)
