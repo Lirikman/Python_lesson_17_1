a0fa143c72f59df1245d90a80c62e2d36b7a4a3c
GolubevDA <999gothicman999@mail.ru>
2023-12-15 15:57:23 +0700

    parsing rabota19.ru

--- Parsing.py
+++ Parsing.py
@@ -1,27 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
 import sqlite3
 
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
 # Парсинг rabota19.ru
 url = 'https://rabota19.ru/vacancy/search/?&page=1'
-response = requests.get(url)
+response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.text, 'lxml')
+
+# Всего страниц на сайте
+page = soup.find_all('a', class_='page')
+all_page = page[-2].text
+print(all_page)
 
 data = soup.find_all('tr')
 
 for i in data:
     try:
+#        sleep(2)
         name = i.find('div', class_='title14 strong').text.strip()
+        salary = i.find('td', class_='title14').text.replace("от", "").replace('до', ' -').strip()
+        description = i.find_all('div', class_='small')[-1].text
+        url = 'https://rabota19.ru' + i.find("a").get("href")
+        print(name + '\n' + salary + '\n' + description + '\n' + url + '\n\n')
     except AttributeError:
         continue
-    salary = i.find('td', class_='title14').text.replace("от", "").replace('до', ' -').strip()
-    description = i.find_all('div', class_='small')[-1].text
-    url = 'https://rabota19.ru' + i.find("a").get("href")
-#    print(name + '\n' + salary + '\n' + description + '\n' + url + '\n\n')
 
-page = soup.find_all('a', class_='page')
-all_page = page[-2].text
-print(all_page)
+
 
 # Создание базы данных SQLite
 connection = sqlite3.connect('my_base.db')
