3c0a89be2072310750ea599295f8e73d3ad597ce
GolubevDA <999gothicman999@mail.ru>
2024-01-09 17:00:25 +0700

    parsing rabota19.ru

--- Parsing.py
+++ Parsing.py
@@ -3,32 +3,67 @@ from bs4 import BeautifulSoup
 from time import sleep
 import sqlite3
 
-
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+
+
 # Парсинг rabota19.ru
-url = 'https://rabota19.ru/vacancy/search/?&page=1'
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, 'lxml')
 
-# Всего страниц на сайте
-page = soup.find_all('a', class_='page')
-all_page = page[-2].text
-print(all_page)
+# Всего страниц
+def get_page():
+    url = 'https://rabota19.ru/vacancy/search/?&page=1'
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    page = soup.find_all('a', class_='page')
+    all_page = page[-2].text
+    return all_page
 
-data = soup.find_all('tr')
 
-for i in data:
+# Список всех ссылок на вакансии
+def get_url():
+    count = 1
+    url = f'https://rabota19.ru/vacancy/search/?&page={count}'
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.find_all('div', class_='title14 strong')
+    for i in data:
+        card_vac = 'https://rabota19.ru' + i.find("a").get('href')
+        yield card_vac
+
+
+# Информация о вакансии
+for card_url in get_url():
+    response = requests.get(card_url, headers=headers)
+    sleep(1)
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.find('div', class_='main-content')
     try:
-#        sleep(2)
-        name = i.find('div', class_='title14 strong').text.strip()
-        salary = i.find('td', class_='title14').text.replace("от", "").replace('до', ' -').strip()
-        description = i.find_all('div', class_='small')[-1].text
-        url = 'https://rabota19.ru' + i.find("a").get("href")
-        print(name + '\n' + salary + '\n' + description + '\n' + url + '\n\n')
-    except AttributeError:
+        name = data.find("h1", class_='red').text.strip()
+        salary = data.find('div', class_='title14').text.replace("от", "").replace('до', ' -').strip()
+        tmp = data.find_all('div', class_='')
+        description = ''
+        tmp_des_1 = data.find_all('div')[4].text.strip()
+        if tmp_des_1 is not None:
+            description += 'График работы: ' + tmp_des_1 + '\n'
+        tmp_des_2 = data.find_all('div')[5].text.strip()
+        if tmp_des_2 is not None:
+            description += tmp_des_2 + '\n'
+        else:
+            tmp_des_3 = data.find_all('div')[6].text.strip()
+            if tmp_des_3 is not None:
+                description += tmp_des_3 + '\n'
+            else:
+                tmp_des_4 = data.find_all('div')[7].text.strip()
+                if tmp_des_4 is not None:
+                    description += tmp_des_4
+        tmp_2 = data.find_all('div', class_='margin3')[1].text.strip()
+        phone = ''
+        if tmp_2.startswith('Телефон: '):
+            phone = tmp_2
+        else:
+            phone = data.find_all('div', class_='margin3')[2].text.strip()
+        print(name + '\n' + salary + '\n' + description + phone + '\n' + card_url + '\n\n')
+    except:
         continue
-
-
 
 # Создание базы данных SQLite
 connection = sqlite3.connect('my_base.db')
